973b9579415eb07cd51a9818e44dd7fb893aad17
Sabeesh Kumar <115797177+sabeeshe@users.noreply.github.com>
2023-09-08 15:54:45 +0530

    fdfsd

--- backEnd/Controllers/Controller.js
+++ backEnd/Controllers/Controller.js
@@ -1,4 +1,4 @@
-const User = require('../Models/userModel');////////////////////
+const User = require('../Models/userModel');////////////////////jgf
 
 //signup api
 exports.signup =async(req, res) => {
