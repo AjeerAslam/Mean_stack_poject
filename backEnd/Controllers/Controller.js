841538296836c2da7dc73b959a2aa06144b8d209
Your Correct Name Here <ajeer@software@associates.in>
2023-09-08 16:23:09 +0530

    fdfa

--- backEnd/Controllers/Controller.js
+++ backEnd/Controllers/Controller.js
@@ -1,4 +1,4 @@
-const User = require('../Models/userModel');////////////////////jgfs
+const User = require('../Models/userModel');////////////////////jgfsdf
 
 //signup api
 exports.signup =async(req, res) => {
